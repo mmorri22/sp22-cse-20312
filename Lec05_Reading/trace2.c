2c030912f393724ac3d244e4d1aa79474e4390e8
Matthew Morrison <mmorri22@nd.edu>
2022-01-09 13:50:59 -0500

    Lecture 05 Update

---
+++ Lec05_Reading/trace2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int func_1( int val );
+int func_2( int val );
+
+int main(){
+    
+    fprintf(stdout, "\n Answer is %d\n", func_1( 5 ) );
+    
+    return 0;
+}
+
+int func_1( int val ){
+    
+    val *= 2;
+    
+    if( val == 0 ){
+        fprintf( stdout, "1 " );
+        return 1;
+    }
+    
+    int return_val = val * func_2( val - 2 );
+    
+    fprintf( stdout, "%d ", return_val );
+    
+    return return_val;
+    
+}
+
+int func_2( int val ){
+
+    val /= 2;
+    
+    if( val <= 2 ){
+        fprintf( stdout, "2 " );
+        return 2;
+    }
+    
+    int return_val = val * func_1( val - 1 );  
+    
+    fprintf( stdout, "%d ", return_val );
+    
+    return return_val;
+
+}
