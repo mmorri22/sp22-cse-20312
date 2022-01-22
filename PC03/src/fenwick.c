56f268dbee82cc505290f6d345ee547320790581
Matthew Morrison <mmorri22@nd.edu>
2022-01-22 13:00:22 -0500

    PC03 Update

---
+++ PC03/src/fenwick.c
@@ -0,0 +1,5 @@
+/** Write your Function Header Information here */
+
+#include "../include/fenwick.h"
+
+/** Write your function code here ***/
--- PC03/src/labyrinth.c
+++
@@ -1,7 +0,0 @@
-#include "../include/labyrinth.h"
-
-int is_path_to_freedom(maze_cell* start, const char* moves, long unsigned int str_length){
-	
-	
-    return 0;
-}
