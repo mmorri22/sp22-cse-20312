631e6bdaf2078294ce11301ff65b5cb4e2a4b544
Matthew Morrison <mmorri22@nd.edu>
2022-01-22 13:05:15 -0500

    PC03 Update

---
+++ PC03/setup.sh
@@ -0,0 +1,12 @@
+wget https://raw.githubusercontent.com/mmorri22/sp22-cse-20312/main/PC03/Makefile
+wget https://raw.githubusercontent.com/mmorri22/sp22-cse-20312/main/PC03/PC03_sample_run.txt
+wget https://raw.githubusercontent.com/mmorri22/sp22-cse-20312/main/PC03/test_PC03.sh
+chmod a+rx test_PC03.sh
+mkdir include src
+cd include
+wget https://raw.githubusercontent.com/mmorri22/sp22-cse-20312/main/PC03/include/fenwick.h
+cd ../src
+wget https://raw.githubusercontent.com/mmorri22/sp22-cse-20312/main/PC03/src/PC03.c
+wget https://raw.githubusercontent.com/mmorri22/sp22-cse-20312/main/PC03/src/fenwick.c
+cd ..
+rm setup.sh
