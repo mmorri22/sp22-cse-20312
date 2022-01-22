87281b86db61b847449d8183a8ed626ee1fc2c5b
Matthew Morrison <mmorri22@nd.edu>
2022-01-22 13:07:42 -0500

    Update PC03 scripts

--- PC03/setup.sh
+++ PC03/setup.sh
@@ -1,4 +1,5 @@
 wget https://raw.githubusercontent.com/mmorri22/sp22-cse-20312/main/PC03/Makefile
+make initialize
 wget https://raw.githubusercontent.com/mmorri22/sp22-cse-20312/main/PC03/PC03_sample_run.txt
 wget https://raw.githubusercontent.com/mmorri22/sp22-cse-20312/main/PC03/test_PC03.sh
 chmod a+rx test_PC03.sh
