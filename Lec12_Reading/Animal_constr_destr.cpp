6ac29a41381cdd9f68fd710d4a243b5d2fd12287
Matthew Morrison <mmorri22@nd.edu>
2022-02-01 09:54:39 -0500

    Update Lecture 12 Reading

--- Lec12_Reading/Animal_constr_destr.cpp
+++ Lec12_Reading/Animal_constr_destr.cpp
@@ -4,10 +4,28 @@
 
 typedef struct Animal {
 
+    public:
         std::string name;
         int age;
+
+    private:
         int weight;
-       
+
+    public:
+    
+        // Default Constructor
+        Animal( ) : name(), age(0), weight(0) {}
+        
+        // Overloaded Constructor 
+        Animal( const std::string& name_in, const int& age_in, const int weight_in ):
+            name( name_in ), age( age_in ), weight( weight_in ) {
+                
+                std::cout << "Constructor Info for: " << this->name << std::endl;
+                std::cout << "Base address  : " << this << std::endl;
+                std::cout << "Name address  : " << &(this->name) << std::endl;
+                std::cout << "Age address   : " << &(this->age) << std::endl;
+                std::cout << "Weight address: " << &(this->weight) << std::endl;
+            }
     
         // to allow access to the private `weight` member via methods, these need to be public
         void setWeight(int w) {
@@ -36,18 +54,14 @@ typedef struct Animal {
 
 int main()
 {
-    Animal stack_cat;
-	stack_cat.name = "kitty";
-	stack_cat.age = 5;
+    Animal stack_cat( "Garfield", 5, 10 );
+    stack_cat.print_Animal();
+    
     stack_cat.setWeight(12);
     stack_cat.print_Animal();    
 
     // C++ Equivalent of Animal* heap_dog = (Animal *)malloc( sizeof(Animal) );
-    Animal* heap_dog = new Animal();
-	heap_dog->name = "Eirinn";
-	heap_dog->age = 7;
-	heap_dog->setWeight(55);
-	
+    Animal* heap_dog = new Animal( "Eirinn", 7, 55);
     heap_dog->print_Animal();
     
     heap_dog->setWeight(999); // will report an error
