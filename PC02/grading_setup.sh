cbd86fd6cd2e5789d7158bed94ed22d28169cd73
Matthew Morrison <mmorri22@nd.edu>
2022-02-07 11:06:07 -0500

    Update PC02 script test

--- PC02/grading_setup.sh
+++ PC02/grading_setup.sh
@@ -38,3 +38,18 @@ wget https://raw.githubusercontent.com/mmorri22/sp22-cse-20312/main/PC02/test6_2
 wget https://raw.githubusercontent.com/mmorri22/sp22-cse-20312/main/PC02/test6_500gen.out
 cd ..
 make PC02
+exe/./PC02 tests/test1.txt 5 outputs/test1_5gen.out
+exe/./PC02 tests/test1.txt 10 outputs/test1_10gen.out
+exe/./PC02 tests/test1.txt 100 outputs/test1_100gen.out
+exe/./PC02 tests/test1.txt 500 outputs/test1_500gen.out
+exe/./PC02 tests/test2.txt 500 outputs/test2_500gen.out
+exe/./PC02 tests/test3.txt 200 outputs/test3_200gen.out
+exe/./PC02 tests/test3.txt 500 outputs/test3_500gen.out
+exe/./PC02 tests/test4.txt 100 outputs/test4_100gen.out
+exe/./PC02 tests/test4.txt 200 outputs/test4_200gen.out
+exe/./PC02 tests/test5.txt 100 outputs/test5_100gen.out
+exe/./PC02 tests/test5.txt 200 outputs/test5_200gen.out
+exe/./PC02 tests/test5.txt 200 outputs/test5_500gen.out
+exe/./PC02 tests/test6.txt 100 outputs/test6_100gen.out
+exe/./PC02 tests/test6.txt 200 outputs/test6_200gen.out
+exe/./PC02 tests/test6.txt 200 outputs/test6_500gen.out
